ea8ee2e4635e6ed64daaf9420ff488e38b819bde
Elvis Ronald Cruz Chullo <teseosoftwarecompany@gmail.com>
2025-04-20 23:45:04 -0500

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import db from "@astrojs/db";
-
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
+// import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +14,6 @@ export default defineConfig({
   integrations: [tailwind(), react()],
 
   output: "server",
-  adapter: netlify(),
+  // adapter: netlify(),
+  adapter: vercel(),
 });
